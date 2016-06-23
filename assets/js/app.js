55d367f639afcdc99a6a686e1a6afc8fff28eb30
Dong Nguyen <ndaidong@gmail.com>
2016-06-23 14:27:23 +0700

    Initial

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+/**
+ * start.js
+ * Init app
+ * @ndaidong
+ */
+
+(() => {
+
+  'use strict';
+
+  var App = Box.Application || {};
+
+  App.on('error', (event) => {
+    var exception = event.exception;
+    console.log(exception); // eslint-disable-line no-console
+  });
+
+  App.init({
+    debug: true
+  });
+
+})();
